ed16ad7ba9da939c551c5b195d6b5192c7bf428f
Ruben de Vries <ruben@rubensayshi.com>
2015-11-13 12:57:00 +0100

    small bash script to update `gh-pages` branch.

---
+++ build-gh-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+gulp
+
+PROJECTDIR=$(pwd)
+TMPDIR=$(uuidgen)
+
+git clone `git config --get remote.origin.url` -b gh-pages /tmp/${TMPDIR} && \
+cd /tmp/${TMPDIR} && \
+cp -rf ${PROJECTDIR}/build/* /tmp/${TMPDIR}/ && \
+git add -A && \
+git commit -am "new build" && \
+git push origin gh-pages  && \
+echo "DONE!"
