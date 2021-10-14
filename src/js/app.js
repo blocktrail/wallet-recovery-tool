47067b3a8b5b17f915cc3e496285d27ea8336d21
jiangjinyuan <937804380@qq.com>
2021-10-14 15:27:35 +0800

    update bsv recovery insight host

--- src/js/app.js
+++ src/js/app.js
@@ -91,7 +91,7 @@ app.controller('walletRecoveryCtrl', function($scope, $q, $modal, $location, $ro
     } else if (window.APPCONFIG.RECOVER_BCC) {
         $scope.recoveryNetwork = {name: "Bitcoin Cash", value: "bcc", testnet: false, insightHost: "https://bch-insight.bitpay.com/api", recoverySheet: false};
     } else if (window.APPCONFIG.RECOVER_BSV) {
-        $scope.recoveryNetwork = {name: "Bitcoin SV", value: "bcc", testnet: false, insightHost: "https://bsv-recovery-proxy.btc.com", recoverySheet: false};
+        $scope.recoveryNetwork = {name: "Bitcoin SV", value: "bcc", testnet: false, insightHost: "https://explorer.api.btc.com/inner/btccom/wallet/bsv", recoverySheet: false};
     } else {
         $scope.recoveryNetwork = null;
     }
